cd43b257c670be612369fdc95046f97f4d7aa227
s1f102200604 <s1f102200604@iniad.org>
2023-12-15 09:58:56 +0900

    repare

--- index.js
+++ index.js
@@ -13,21 +13,28 @@ fetch(url)
     document.getElementById("temp_max").innerHTML = 'Maximum temperature:' + (data.main.temp_max - 273.2).toPrecision(3) + '℃';
     document.getElementById("temp_min").innerHTML = 'Lowest temperature:' + (data.main.temp_min - 273.2).toPrecision(3) + '℃';
     document.getElementById("humidity").innerHTML = 'Humidity:' + data.main.humidity + '%';
-    displayWeatherImage(data.weather[0].main);
-})
+    displayWeatherImage(data.weather[0].main); 
+    //document.getElementById("description").innerHTML = data.weather[0].description; // 天気の詳細
+    document.getElementById("sunrise").innerHTML = Date(data.sys.sunrise * 1000).toLocaleTimeString(); // 日の出
+    document.getElementById("sunset").innerHTML = Date(data.sys.sunset * 1000).toLocaleTimeString(); // 日の入り
+    document.getElementById("speed").innerHTML = data.wind.speed; // 風速
+    document.getElementById("deg").innerHTML = data.wind.deg; // 風向
+    document.getElementById("pressure").innerHTML = data.main.pressure; // 大気圧
+    document.getElementById("clouds").innerHTML = data.clouds.all; // 雲量
+  });
 
-function displayWeatherImage(weatherDescription) {
+function displayWeatherImage(weatherMain) {
     const weatherImageContainer = document.getElementById('weatherImage');
     const imgFolderPath = 'img/';
   
     // 天気に応じた画像ファイル名を取得
-    const imageName = weatherDescription.toLowerCase().replace(' ', '_') + '.png';
+    const imageName = weatherMain.toLowerCase().replace(' ', '_') + '.png';
     const imageUrl = imgFolderPath + imageName;
   
     // 画像を表示
     const weatherImage = document.createElement('img');
     weatherImage.src = imageUrl;
-    weatherImage.alt = weatherDescription;
+    weatherImage.alt = weatherMain;
     weatherImageContainer.innerHTML = '';
     weatherImageContainer.appendChild(weatherImage);
   }
