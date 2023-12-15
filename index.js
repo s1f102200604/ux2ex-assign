214e6bdb121626145851971aa6ee9d7794c12099
s1f102200604 <s1f102200604@iniad.org>
2023-12-15 16:27:40 +0900

    各情報の掲載、画像の統一化

--- index.js
+++ index.js
@@ -8,33 +8,35 @@ const url = URL + CITY + '&appid=' + API_KEY_WEATHER;
 fetch(url)
 .then(response => response.json())
 .then((data) => {
-    document.getElementById("name").innerHTML = 'City:' + data.name;
-    document.getElementById("temp").innerHTML = 'Temperature:' + (data.main.temp - 273.2).toPrecision(3) + '℃';
-    document.getElementById("temp_max").innerHTML = 'Maximum temperature:' + (data.main.temp_max - 273.2).toPrecision(3) + '℃';
-    document.getElementById("temp_min").innerHTML = 'Lowest temperature:' + (data.main.temp_min - 273.2).toPrecision(3) + '℃';
-    document.getElementById("humidity").innerHTML = 'Humidity:' + data.main.humidity + '%';
+    document.getElementById("name").innerHTML = data.name;
+    document.getElementById("temp").innerHTML = (data.main.temp - 273.2).toPrecision(3) + '℃';
+    document.getElementById("temp_max").innerHTML = (data.main.temp_max - 273.2).toPrecision(3) + '℃';
+    document.getElementById("temp_min").innerHTML = (data.main.temp_min - 273.2).toPrecision(3) + '℃';
+    document.getElementById("humidity").innerHTML = data.main.humidity + '%';
     displayWeatherImage(data.weather[0].main); 
-    //document.getElementById("description").innerHTML = data.weather[0].description; // 天気の詳細
-    document.getElementById("sunrise").innerHTML = Date(data.sys.sunrise * 1000).toLocaleTimeString(); // 日の出
-    document.getElementById("sunset").innerHTML = Date(data.sys.sunset * 1000).toLocaleTimeString(); // 日の入り
-    document.getElementById("speed").innerHTML = data.wind.speed; // 風速
-    document.getElementById("deg").innerHTML = data.wind.deg; // 風向
-    document.getElementById("pressure").innerHTML = data.main.pressure; // 大気圧
-    document.getElementById("clouds").innerHTML = data.clouds.all; // 雲量
+    document.getElementById("description").innerHTML = data.weather[0].description;//詳細天気
+    document.getElementById("sunrise").innerHTML = (new Date(data.sys.sunrise * 1000)).toLocaleTimeString(); // 日の出
+    document.getElementById("sunset").innerHTML = (new Date(data.sys.sunset * 1000)).toLocaleTimeString(); // 日の入り
+    document.getElementById("speed").innerHTML = data.wind.speed + 'm/s';//風速
+    document.getElementById("deg").innerHTML = convertWindDirection(data.wind.deg);//風向
+    document.getElementById("pressure").innerHTML = data.main.pressure + 'hPa'; // 大気圧
+    document.getElementById("clouds_amount").innerHTML = data.clouds.all + '%'; // 雲量
   });
 
-function displayWeatherImage(weatherMain) {
+  function displayWeatherImage(weatherMain) {
     const weatherImageContainer = document.getElementById('weatherImage');
     const imgFolderPath = 'img/';
-  
-    // 天気に応じた画像ファイル名を取得
-    const imageName = weatherMain.toLowerCase().replace(' ', '_') + '.png';
+    const imageName = weatherMain.toLowerCase() + '.png';
     const imageUrl = imgFolderPath + imageName;
-  
-    // 画像を表示
     const weatherImage = document.createElement('img');
     weatherImage.src = imageUrl;
     weatherImage.alt = weatherMain;
     weatherImageContainer.innerHTML = '';
     weatherImageContainer.appendChild(weatherImage);
+  }
+
+  function convertWindDirection(degrees) {
+    var directions = ["北", "北北東", "北東", "東北東", "東", "東南東", "南東", "南南東", "南", "南南西", "南西", "西南西", "西", "西北西", "北西", "北北西"];
+    var index = Math.round((degrees % 360) / 22.5);
+    return directions[(index + 16) % 16];
   }
